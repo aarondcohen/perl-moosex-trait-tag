9a6a524de89d83b48ea83654fb78025a821ba71c
Aaron Cohen <aarondcohen@gmail.com>
2013-11-17 18:53:17 -0500

    Renamed MooseX::Trait::Label to MooseX::Trait::Tag

--- lib/MooseX/Trait/Tag.pm
+++ lib/MooseX/Trait/Tag.pm
@@ -1,8 +1,8 @@
-package MooseX::Trait::Label;
+package MooseX::Trait::Tag;
 
 =head1 NAME
 
-MooseX::Trait::Label - give a label to an attribute
+MooseX::Trait::Tag - Add an arbitrary tag to an attribute
 
 =cut
 
@@ -19,16 +19,16 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-This module provides a way to give a Moose attribute an arbitrary label.
+This module provides a way to give a Moose attribute an arbitrary tag.
 Various methods are installed into the package declaring the attribute,
-allowing for actions to be taken on only the labeled attributes.
+allowing for actions to be taken on only the tagged attributes.
 This module is inspired by L<http://search.cpan.org/~ether/Moose/lib/Moose/Cookbook/Meta/Labeled_AttributeTrait.pod>
 
 Example usage:
 
 	package Foo;
 	use Moose;
-	use MooseX:Trait::Label qw{metadata};
+	use MooseX:Trait::Tag qw{metadata};
 
 	has field1 => (is => 'rw', traits => [qw{metadata}]);
 	has field2 => (is => 'ro', traits => [qw{metadata}]);
@@ -63,56 +63,66 @@ Example usage:
 sub import {
 	my $class = shift;
 	my $importing_class = caller();
-	$class->register_label(importing_class => $importing_class, label => $_) for @_;
+	$class->register_tag(importing_class => $importing_class, tag => $_) for @_;
 }
 
-sub register_label {
+=head2 register_tag(importing_class => $importing_class, tag => $tag)
+
+Install the methods asociated with the tag into the importing class.
+
+=cut
+
+sub register_tag {
 	my $class = shift;
 	my %args = @_;
-	my ($importing_class, $label) = @args{qw{importing_class label}};
+	my ($importing_class, $tag) = @args{qw{importing_class tag}};
 
 	#Moose magic to create a new trait bound to the label
-	my $label_class = "$class\::$label";
-	Moose::Meta::Role->initialize($label_class);
-	Moose::Exporter->setup_import_methods(exporting_package => $label_class);
-	Moose::Util::meta_attribute_alias($label, $label_class);
+	my $tag_class = "$class\::$tag";
+	Moose::Meta::Role->initialize($tag_class);
+	Moose::Exporter->setup_import_methods(exporting_package => $tag_class);
+	Moose::Util::meta_attribute_alias($tag, $tag_class);
 
-=head2 is_attribute_<label>( $attribute_name )
+=head1 INSTALLED METHODS
 
-Given an attribute name, determine if it is registered to the label.
+=cut
+
+=head2 is_attribute_<tag>( $attribute_name )
+
+Given an attribute name, determine if it is registered to the tag.
 Requires an attribute name.
 
 =cut
 
 	my $importing_class_meta = $importing_class->meta;
-	$importing_class_meta->add_method("is_attribute_$label", sub {
+	$importing_class_meta->add_method("is_attribute_$tag", sub {
 		my $attribute = (shift)->meta->find_attribute_by_name(@_);
-		return $attribute && $attribute->does($label);
-	}) unless $importing_class_meta->has_method("is_attribute_$label");
+		return $attribute && $attribute->does($tag);
+	}) unless $importing_class_meta->has_method("is_attribute_$tag");
 
-=head2 <label>_attributes( )
+=head2 <tag>_attributes( )
 
-Returns the names of all attributes marked with the label.
+Returns the names of all attributes marked with the tag.
 
 =cut
 
-	$importing_class_meta->add_method("$label\_attributes", sub {
+	$importing_class_meta->add_method("$tag\_attributes", sub {
 		map { $_->name }
-		grep { $_->does($label) }
+		grep { $_->does($tag) }
 		(shift)->meta->get_all_attributes
-	}) unless $importing_class_meta->has_method("$label\_attributes");
+	}) unless $importing_class_meta->has_method("$tag\_attributes");
 
-=head2 update_<label>( attribute1 => $new_value1, ... )
+=head2 update_<tag>( attribute1 => $new_value1, ... )
 
 Given name-value pairs, update each writable attribute with the new value
-if it is associated with the appropriate label.
+if it is associated with the appropriate tag.
 
 =cut
 
-	$importing_class_meta->add_method("update_$label", sub {
+	$importing_class_meta->add_method("update_$tag", sub {
 		my ($self, %args) = @_;
 
-		for my $attribute (grep { $_->does($label) } $self->meta->get_all_attributes) {
+		for my $attribute (grep { $_->does($tag) } $self->meta->get_all_attributes) {
 			next unless exists $args{$attribute->name};
 			my $writer = $attribute->get_write_method;
 			next unless $writer;
@@ -120,7 +130,7 @@ if it is associated with the appropriate label.
 		}
 
 		return;
-	}) unless $importing_class_meta->has_method("update_$label");
+	}) unless $importing_class_meta->has_method("update_$tag");
 }
 
 1;
--- t/basics.t
+++ t/basics.t
@@ -9,13 +9,13 @@ use lib "$FindBin::Bin/../lib/";
 use Set::Functional;
 use Test::Most tests => 13;
 
-use_ok 'MooseX::Trait::Label';
+use_ok 'MooseX::Trait::Tag';
 
 {
 	package Foo;
 	use Moose;
 
-	use MooseX::Trait::Label qw{metadata tag};
+	use MooseX::Trait::Tag qw{metadata tag};
 
 	has bar => (
 		traits => [qw/metadata/],
@@ -38,7 +38,7 @@ use_ok 'MooseX::Trait::Label';
 	package Bare;
 	use Moose;
 
-	use MooseX::Trait::Label qw{nothing};
+	use MooseX::Trait::Tag qw{nothing};
 
 	has bar => (
 		is => 'rw',
@@ -51,15 +51,15 @@ use_ok 'MooseX::Trait::Label';
 my $foo = Foo->new();
 my $bare = Bare->new();
 
-cmp_set [$foo->metadata_attributes], [qw{bar baz}], '<label>_attributes find all labeled attributes';
-cmp_set [Set::Functional::intersection [$foo->metadata_attributes], [$foo->tag_attributes]], [qw{baz}], 'Multiple labels on an attribute are orthogonal to eachother';
-ok $foo->is_attribute_metadata('bar'), 'is_attribute_<label> identifies labeled attribute as labeled';
-ok ! $foo->is_attribute_metadata('bam'), 'is_attribute_<label> identifies unlabeled attribute as not labeled';
-ok ! $foo->is_attribute_metadata('none'), 'is_attribute_<label> identifies non-existent attribute as not labeled';
-dies_ok { $foo->is_attribute_metadata() } 'is_attribute_<label> fails when not given an attribute';
-lives_ok { $foo->update_metadata(bar => 235, baz => 789, bam => 'wont update', none => 'also wont update') } 'update_<labeled> handles all input';
-is $foo->bar, 235, 'update_<label> modifies writable labeled attributes';
-is $foo->baz, 94796, 'update_<label> does not modify read-only labeled attributes';
-is $foo->bam, 94745, 'update_<label> does not modify unlabled attributes';
-throws_ok { $bare->metadata_attributes } qr/Can't locate object method/, 'Injecting a label does not pollute other classes';
-lives_ok { $bare->nothing_attributes; $bare->is_attribute_nothing('nothing'); $bare->update_nothing } 'Injecting a label adds all appropriate methods to the importing class, regardless of the attributes';
+cmp_set [$foo->metadata_attributes], [qw{bar baz}], '<tag>_attributes find all tagged attributes';
+cmp_set [Set::Functional::intersection [$foo->metadata_attributes], [$foo->tag_attributes]], [qw{baz}], 'Multiple tags on an attribute are orthogonal to eachother';
+ok $foo->is_attribute_metadata('bar'), 'is_attribute_<tag> identifies tagged attribute as tagged';
+ok ! $foo->is_attribute_metadata('bam'), 'is_attribute_<tag> identifies untagged attribute as not tagged';
+ok ! $foo->is_attribute_metadata('none'), 'is_attribute_<tag> identifies non-existent attribute as not tagged';
+dies_ok { $foo->is_attribute_metadata() } 'is_attribute_<tag> fails when not given an attribute';
+lives_ok { $foo->update_metadata(bar => 235, baz => 789, bam => 'wont update', none => 'also wont update') } 'update_<tag> handles all input';
+is $foo->bar, 235, 'update_<tag> modifies writable tagged attributes';
+is $foo->baz, 94796, 'update_<tag> does not modify read-only tagged attributes';
+is $foo->bam, 94745, 'update_<tag> does not modify untagged attributes';
+throws_ok { $bare->metadata_attributes } qr/Can't locate object method/, 'Injecting a tag does not pollute other classes';
+lives_ok { $bare->nothing_attributes; $bare->is_attribute_nothing('nothing'); $bare->update_nothing } 'Injecting a tag adds all appropriate methods to the importing class, regardless of the attributes';
