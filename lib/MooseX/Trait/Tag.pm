961d833c74815e969444be5a30d52e7847fef28c
Aaron Cohen <aarondcohen@gmail.com>
2013-11-18 05:12:53 -0500

    Final preparations for open-sourcing
    - added get_<tag> method
    - converted <tag>_attributes to all_<tag>
    - converted update_<tag> to set_<tag>
    - added support documentation
    - cahnged version to 1.00

--- lib/MooseX/Trait/Tag.pm
+++ lib/MooseX/Trait/Tag.pm
@@ -15,7 +15,7 @@ Version 0.01
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '1.00';
 
 =head1 SYNOPSIS
 
@@ -87,7 +87,7 @@ sub register_tag {
 
 =cut
 
-=head2 is_attribute_<tag>( $attribute_name )
+=head2 is_<tag>( $attribute_name )
 
 Given an attribute name, determine if it is registered to the tag.
 Requires an attribute name.
@@ -95,31 +95,46 @@ Requires an attribute name.
 =cut
 
 	my $importing_class_meta = $importing_class->meta;
-	$importing_class_meta->add_method("is_attribute_$tag", sub {
+	$importing_class_meta->add_method("is_$tag", sub {
 		my $attribute = (shift)->meta->find_attribute_by_name(@_);
 		return $attribute && $attribute->does($tag);
-	}) unless $importing_class_meta->has_method("is_attribute_$tag");
+	}) unless $importing_class_meta->has_method("is_$tag");
 
-=head2 <tag>_attributes( )
+=head2 all_<tag>( )
 
-Returns the names of all attributes marked with the tag.
+Return the names of all attributes marked with the tag.
 
 =cut
 
-	$importing_class_meta->add_method("$tag\_attributes", sub {
+	$importing_class_meta->add_method("all_$tag", sub {
 		map { $_->name }
 		grep { $_->does($tag) }
 		(shift)->meta->get_all_attributes
-	}) unless $importing_class_meta->has_method("$tag\_attributes");
+	}) unless $importing_class_meta->has_method("all_$tag");
 
-=head2 update_<tag>( attribute1 => $new_value1, ... )
+=head2 get_<tag>( )
+
+Return all name-value pairs for each readable attribute associated with the
+appropriate tag.
+
+=cut
+
+	$importing_class_meta->add_method("get_$tag", sub {
+		my $self = shift;
+		return
+			map { my $reader = $_->get_read_method; ($_->name => $self->$reader) }
+			grep { $_->does($tag) && $_->get_read_method }
+			$self->meta->get_all_attributes
+	}) unless $importing_class_meta->has_method("get_$tag");
+
+=head2 set_<tag>( attribute1 => $new_value1, ... )
 
 Given name-value pairs, update each writable attribute with the new value
 if it is associated with the appropriate tag.
 
 =cut
 
-	$importing_class_meta->add_method("update_$tag", sub {
+	$importing_class_meta->add_method("set_$tag", sub {
 		my ($self, %args) = @_;
 
 		for my $attribute (grep { $_->does($tag) } $self->meta->get_all_attributes) {
@@ -130,7 +145,64 @@ if it is associated with the appropriate tag.
 		}
 
 		return;
-	}) unless $importing_class_meta->has_method("update_$tag");
+	}) unless $importing_class_meta->has_method("set_$tag");
 }
 
-1;
+=head1 ACKNOWLEDGEMENTS
+
+This module is brought to you by L<Shutterstock|http://www.shutterstock.com/>
+(L<@ShutterTech|https://twitter.com/ShutterTech>).  Additional open source
+projects from Shutterstock can be found at
+L<code.shutterstock.com|http://code.shutterstock.com/>.
+
+=head1 AUTHOR
+
+Aaron Cohen, C<< <aarondcohen at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-moosex-trait-tag at rt.cpan.org>, or through
+the web interface at L<https://github.com/aarondcohen/MooseX-Trait-Tag/issues>.  I will
+be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MooseX::Trait::Tag
+
+You can also look for information at:
+
+=over 4
+
+=item * Official GitHub Repo
+
+L<https://github.com/aarondcohen/MooseX-Trait-Tag>
+
+=item * GitHub's Issue Tracker (report bugs here)
+
+L<https://github.com/aarondcohen/MooseX-Trait-Tag/issues>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Set-Functional>
+
+=item * Official CPAN Page
+
+L<http://search.cpan.org/dist/MooseX-Trait-Tag/>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Aaron Cohen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of MooseX::Trait::Tag
--- t/basics.t
+++ t/basics.t
@@ -7,7 +7,7 @@ use FindBin;
 use lib "$FindBin::Bin/../lib/";
 
 use Set::Functional;
-use Test::Most tests => 13;
+use Test::Most tests => 14;
 
 use_ok 'MooseX::Trait::Tag';
 
@@ -51,15 +51,16 @@ use_ok 'MooseX::Trait::Tag';
 my $foo = Foo->new();
 my $bare = Bare->new();
 
-cmp_set [$foo->metadata_attributes], [qw{bar baz}], '<tag>_attributes find all tagged attributes';
-cmp_set [Set::Functional::intersection [$foo->metadata_attributes], [$foo->tag_attributes]], [qw{baz}], 'Multiple tags on an attribute are orthogonal to eachother';
-ok $foo->is_attribute_metadata('bar'), 'is_attribute_<tag> identifies tagged attribute as tagged';
-ok ! $foo->is_attribute_metadata('bam'), 'is_attribute_<tag> identifies untagged attribute as not tagged';
-ok ! $foo->is_attribute_metadata('none'), 'is_attribute_<tag> identifies non-existent attribute as not tagged';
-dies_ok { $foo->is_attribute_metadata() } 'is_attribute_<tag> fails when not given an attribute';
-lives_ok { $foo->update_metadata(bar => 235, baz => 789, bam => 'wont update', none => 'also wont update') } 'update_<tag> handles all input';
-is $foo->bar, 235, 'update_<tag> modifies writable tagged attributes';
-is $foo->baz, 94796, 'update_<tag> does not modify read-only tagged attributes';
-is $foo->bam, 94745, 'update_<tag> does not modify untagged attributes';
-throws_ok { $bare->metadata_attributes } qr/Can't locate object method/, 'Injecting a tag does not pollute other classes';
-lives_ok { $bare->nothing_attributes; $bare->is_attribute_nothing('nothing'); $bare->update_nothing } 'Injecting a tag adds all appropriate methods to the importing class, regardless of the attributes';
+cmp_set [$foo->all_metadata], [qw{bar baz}], 'all_<tag> find all tagged attributes';
+cmp_set [Set::Functional::intersection [$foo->all_metadata], [$foo->all_tag]], [qw{baz}], 'Multiple tags on an attribute are orthogonal to eachother';
+ok $foo->is_metadata('bar'), 'is_<tag> identifies tagged attribute as tagged';
+ok ! $foo->is_metadata('bam'), 'is_<tag> identifies untagged attribute as not tagged';
+ok ! $foo->is_metadata('none'), 'is_<tag> identifies non-existent attribute as not tagged';
+dies_ok { $foo->is_metadata() } 'is_<tag> fails when not given an attribute';
+is_deeply {$foo->get_metadata}, {bar => 3573573, baz => 94796}, 'get_<tag> returns they key-value pairs of thetagged attributes';
+lives_ok { $foo->set_metadata(bar => 235, baz => 789, bam => 'wont update', none => 'also wont update') } 'set_<tag> handles all input';
+is $foo->bar, 235, 'set_<tag> modifies writable tagged attributes';
+is $foo->baz, 94796, 'set_<tag> does not modify read-only tagged attributes';
+is $foo->bam, 94745, 'set_<tag> does not modify untagged attributes';
+throws_ok { $bare->all_metadata } qr/Can't locate object method/, 'Injecting a tag does not pollute other classes';
+lives_ok { $bare->all_nothing; $bare->is_nothing('nothing'); $bare->get_nothing; $bare->set_nothing } 'Injecting a tag adds all appropriate methods to the importing class, regardless of the attributes';
